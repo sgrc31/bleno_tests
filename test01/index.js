6833a8e627dc63c9bae86fbc4d7dbd578cf2b50a
Simone Guercio <s.guercio@gmail.com>
2017-10-05 16:36:02 +0200

    scemo scemo scemo

--- test01/index.js
+++ test01/index.js
@@ -10,23 +10,23 @@ console.log('inizializzato bleno');
 var descriptorA1 = new BlenoDescriptor({
     uuid: '2901',
     value: 'valoreA1'
-)};
+});
 
 var descriptorA2 = new BlenoDescriptor({
     uuid: '2902',
     value: 'valoreA2'
-)};
+});
 
 var caratteristica1 = new BlenoCharacteristic({
     uuid: 'fffffffffffffffffffffffffffffff1',
     properties: ['read', 'write'],
     //secure: ['read', 'write'],
-    descriptors: [ 'descriptorA1', 'descriptorA2' ],
+    descriptors: [ descriptorA1, descriptorA2 ],
 });
 
 var mioPrimaryService = new BlenoPrimaryService({
     uuid: 'fffffffffffffffffffffffffffffff0',
-    characteristics: [ 'caratteristica1' ]
+    characteristics: [ caratteristica1 ]
 });
 
 bleno.on('stateChange', function(state) {
