16572742c17d975b73a922d55438c33168100cad
Simone Guercio <s.guercio@gmail.com>
2017-11-09 16:51:35 +0100

    Initial release quietude gatt server

--- test01/index.js
+++ test01/index.js
@@ -1,50 +1,73 @@
 var bleno = require('bleno');
 
-
-var BlenoPrimaryService = bleno.PrimaryService;
-var BlenoCharacteristic = bleno.Characteristic;
-var BlenoDescriptor = bleno.Descriptor;
-
-console.log('inizializzato bleno');
-
-var descriptorA1 = new BlenoDescriptor({
-    uuid: 'b707df58-ce1b-4b47-8bf9-9a6b80812f1a',
-    value: 'valoreA1'
-});
-
-var descriptorA2 = new BlenoDescriptor({
-    uuid: '7e8dd020-6bc7-43d3-a1f0-37b954cceee6',
-    value: 'valoreA2'
-});
-
-var caratteristica1 = new BlenoCharacteristic({
-    uuid: 'fffffffffffffffffffffffffffffff1',
+var charSettings = new bleno.Characteristic({
+    uuid: 'fffffffffffffffffffffffffffffff2',
     properties: ['read', 'write'],
-    //secure: ['read', 'write'],
-    descriptors: [ descriptorA1, descriptorA2 ],
+    value: '1,3,6',
+    onReadRequest: function(offset, callback) {
+        console.log('Effettuata lettura su settings');
+    },
+    onWriteRequest: function(data, offset, withoutResponse, callback) {
+        console.log('Effettuata scrittura su settings= ' + data.toString('hex'));
+        callback(this.RESULT_SUCCESS);
+    }
 });
 
-var mioPrimaryService = new BlenoPrimaryService({
+var charIdSuoniCaptati = new bleno.Characteristic({
+    uuid: 'fffffffffffffffffffffffffffffff3',
+    properties: ['write'],
+    value: null,
+    onWriteRequest: function(data, offset, withoutResponse, callback) {
+        console.log('Effettuata scrittura su id suono registrato= ' + data.toString('hex'));
+        callback(this.RESULT_SUCCESS);
+    }
+});
+
+var MioServizioPrimario = new bleno.PrimaryService({
     uuid: 'fffffffffffffffffffffffffffffff0',
-    characteristics: [ caratteristica1 ]
+    characteristics: [charSettings, charIdSuoniCaptati]
 });
 
+
+//=====================
+// Registro servizi allo start dell'advertising
+//==================================
+bleno.on('advertisingStart', function(error) {
+    console.log('on -> advertisingStart: ' + (error ? 'error ' + error : 'success'));
+
+    if (!error) {
+        bleno.setServices([MioServizioPrimario]);
+    }
+});
+
+bleno.on('advertisingStop', function() {
+  console.log('on -> advertisingStop');
+});
+
+bleno.on('servicesSet', function(error) {
+  console.log('on -> servicesSet: ' + (error ? 'error ' + error : 'success'));
+});
+
+//notifiche varie e anche poco utili
+bleno.on('accept', function(clientAddress) {
+  console.log('on -> accept, client: ' + clientAddress);
+
+  bleno.updateRssi();
+});
+
+bleno.on('disconnect', function(clientAddress) {
+  console.log('on -> disconnect, client: ' + clientAddress);
+});
+
+//====================
+// Inizio advertising
+//=====================
 bleno.on('stateChange', function(state) {
   console.log('on -> stateChange: ' + state + ', address = ' + bleno.address);
 
   if (state === 'poweredOn') {
-    bleno.startAdvertising('miogatt', ['fffffffffffffffffffffffffffffff0']);
+    bleno.startAdvertising('Quietude', ['fffffffffffffffffffffffffffffff0']);
   } else {
     bleno.stopAdvertising();
-  }
-});
-
-bleno.on('advertisingStart', function(error) {
-  console.log('on -> advertisingStart: ' + (error ? 'error ' + error : 'success'));
-
-  if (!error) {
-    bleno.setServices([
-      mioPrimaryService
-    ]);
   }
 });
